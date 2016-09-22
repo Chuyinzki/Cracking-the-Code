c1a8a1cfca11b5e333c61ac1bf6da31e7a48c1c2
Jesus Villegas <jesus_villegas@intuit.com>
2016-09-21 19:49:34 -0700

    solution to 1_4

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
 import solutions.Problem1_1;
 import solutions.Problem1_3;
+import solutions.Problem1_4;
+
+import java.util.AbstractMap;
 
 public class Main {
 
     public static void main(String[] args) {
-        String testId = "1_3";
+        String testId = "1_4";
         switch (testId) {
             case "1_1":
                 String[] strings = {"yolo", "homie", "github", "google", "jumanji", "", "f", "koala", "8iekd"};
@@ -22,6 +25,13 @@ public class Main {
                     System.out.println(String.format("Strings \"%s\" and \"%s\" are%s permutations", stringArr[0], stringArr[1],
                             Problem1_3.isPermutation2(stringArr[0], stringArr[1]) ? "" : " not"));
                 }
+                break;
+            case "1_4":
+                AbstractMap.SimpleImmutableEntry[] pairs = {new AbstractMap.SimpleImmutableEntry<>("bla h  ", 5),
+                new AbstractMap.SimpleImmutableEntry<>("Mr John Smith    ", 13)};
+                for(AbstractMap.SimpleImmutableEntry pair : pairs)
+                    System.out.println(String.format("\"%s\" turns to \"%s\"", pair.getKey(),
+                            Problem1_4.removeSpaces((String)pair.getKey(), (int)pair.getValue())));
         }
     }
 }
